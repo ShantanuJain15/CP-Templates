87f705308e14b30953ea9e17901262de3de29b04
ShantanuJain15 <94964741+ShantanuJain15@users.noreply.github.com>
2024-01-25 18:17:23 +0530

    Create main_template.cpp
    main template

---
+++ main_template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+
+int main(){
+    // fastio();
+    int tc;
+    cin >>tc;
+    
+     while(tc--){
+    
+}
+
