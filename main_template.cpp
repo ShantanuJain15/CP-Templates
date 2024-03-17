27c85b4b825701c8d09f1af42bd0d22f2dc665d5
Shantanu Jain <shantanujain1507@gmail.com>
2024-03-18 00:37:12 +0530

    add modinverse and print function

--- main_template.cpp
+++ main_template.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
@@ -15,18 +14,24 @@ using namespace std;
 #define ss second
 
 typedef long long ll;
-typedef __int128 xll;
+typedef unsigned long ull;
+// typedef __int128 xll;
+
+template<class T> void _print(T t){cerr<<t;}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
 
 ll power(ll x, ll y,ll mod) {  ll ans =1ll; while(y>0){ if(y&1)ans=(ans*x)%mod; x=(x*x)%mod;y=y>>1;} return ans;}
 ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
 vector<ll> sieve(int n){vector <ll> prime; vector<bool> is_prime(n+1, true);is_prime[0] = is_prime[1] = false; for (int i = 2; i  <= n; i++) {if (is_prime[i] ){prime.pb(i); if( (ll)i * i <= n)  for(int j = i * i; j <= n; j += i) is_prime[j] = false;}} return prime;}
+ll modinverse(ll a,ll p){return power(a,p-2,p);}// p can only be a prime number.
+
+void solve(){
+    return;
+}
 int main(){
-    // fastio();
+    fastio();
     int tc;
     cin >>tc;
-    
-     while(tc--){
-    
-}
-    
+     while(tc--)solve();
 }
